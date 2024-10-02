bdd8a197509931a9da1ecd0bccf8e932e4974f8d
Andrey Malyar <malyar.andrey@list.ru>
2024-10-02 09:58:17 +0500

    feat: button top-btn + style + script

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ document.querySelectorAll(".tabs-trigger__item").forEach(item => {
         document.getElementById(id).classList.add("tabs-content__item_active");
     })
 })
+
+//////////
+// main button top-btn
+//////////
+const topBtn = document.querySelector(".top-btn");
+topBtn.addEventListener("click", toTop);
+
+function toTop() {
+    let scrolled = window.scrollY;
+    scrolled -= 50;
+    window.scrollTo(0, scrolled);
+    if(scrolled > 0) {
+        // topBtn.style.opacity = '0';
+        setTimeout(toTop, 20)
+    } else {
+        // topBtn.style.opacity = '1';
+    }
+}
+window.addEventListener("scroll", () => {
+    if (scrollY > 500) { topBtn.style.display = 'flex'; }
+    else { topBtn.style.display = 'none'; }
+});
